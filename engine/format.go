c63b3cc18fb299073fc578c6ed9180029d1f78a9
Guillaume Denis <gdenispro@gmail.com>
2023-06-12 16:52:15 +0200

    split engine files

---
+++ engine/format.go
@@ -0,0 +1,133 @@
+package engine
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/ducksouplab/ducksoup/store"
+	"github.com/pion/interceptor"
+	"github.com/pion/rtcp"
+	"github.com/rs/zerolog/log"
+)
+
+// used for packet dumps
+
+func rtcpFormatSent(pkts []rtcp.Packet, _ interceptor.Attributes) (res string) {
+	for _, pkt := range pkts {
+		switch rtcpPacket := pkt.(type) {
+		case *rtcp.TransportLayerNack:
+			res += fmt.Sprintf(
+				"[NACK] ssrc:%v sender:%v nacks:%v",
+				rtcpPacket.MediaSSRC,
+				rtcpPacket.SenderSSRC,
+				rtcpPacket.Nacks,
+			)
+		case *rtcp.TransportLayerCC:
+			var count uint16 = 0
+			var lost uint16 = 0
+			for _, chunk := range rtcpPacket.PacketChunks {
+				switch chk := chunk.(type) {
+				case *rtcp.RunLengthChunk:
+					count += chk.RunLength
+					if chk.PacketStatusSymbol == 0 {
+						lost += chk.RunLength
+					}
+				case *rtcp.StatusVectorChunk:
+					for _, symbol := range chk.SymbolList {
+						count += 1
+						if symbol == 0 {
+							lost += 1
+						}
+					}
+				}
+			}
+			res += fmt.Sprintf(
+				"[TWCC] ssrc:%v count:%v lost:%v",
+				rtcpPacket.MediaSSRC,
+				count,
+				lost,
+			)
+
+		case *rtcp.ReceiverReport:
+			res += "[RR sent] reports: "
+			for _, report := range rtcpPacket.Reports {
+				res += fmt.Sprintf("lost=%d/%d ", report.FractionLost, report.TotalLost)
+			}
+		default:
+			res += fmt.Sprintf("[%T sent]", rtcpPacket)
+		}
+	}
+	return res
+}
+
+// func formatReceivedRTCP(pkts []rtcp.Packet, _ interceptor.Attributes) (res string) {
+// 	for _, pkt := range pkts {
+// 		res += fmt.Sprintf("[receiver] %T RTCP packet: %v\n", pkt, pkt)
+// 	}
+// 	return res
+// }
+
+// func formatReceivedRTP(pkt *rtp.Packet, attributes interceptor.Attributes) string {
+// 	var twcc rtp.TransportCCExtension
+// 	ext := pkt.GetExtension(pkt.GetExtensionIDs()[0])
+// 	twcc.Unmarshal(ext)
+
+// 	return fmt.Sprintf("[RTP] #%v now:%v ssrc:%v size:%v",
+// 		twcc.TransportSequence,
+// 		time.Now().Format("15:04:05.99999999"),
+// 		pkt.SSRC,
+// 		pkt.MarshalSize(),
+// 	)
+// }
+
+// used by RTCP log interceptors
+type logWriteCloser struct{}
+
+func (wc *logWriteCloser) Write(p []byte) (n int, err error) {
+	n = len(p)
+	if n > 0 {
+		msg := string(p)
+		if strings.HasPrefix(msg, "[TWCC]") {
+			ssrcMatch := ssrcRegexp.FindStringSubmatch(msg)
+			// trace level to respect DUCKSOUP_LOG_LEVEL setting
+			if len(ssrcMatch) > 0 {
+				// remove ssrc from string and add it as a log prop
+				msg = ssrcRegexp.ReplaceAllString(msg, "")
+
+				if ssrc64, err := strconv.ParseUint(ssrcMatch[1], 10, 32); err == nil {
+					ssrc := uint32(ssrc64)
+					ssrcLog := store.GetFromSSRCIndex(ssrc)
+					if ssrcLog != nil {
+						countMatch := countRegexp.FindStringSubmatch(msg)
+						lostMatch := lostRegexp.FindStringSubmatch(msg)
+
+						if len(countMatch) > 0 && len(lostMatch) > 0 {
+							// don't log empty counts
+							if count, err := strconv.ParseUint(countMatch[1], 10, 64); err == nil && count != 0 {
+								if lost, err := strconv.ParseUint(lostMatch[1], 10, 64); err == nil {
+									log.Logger.Trace().
+										Str("context", "track").
+										Str("ssrc", ssrcMatch[1]).
+										Str("namespace", ssrcLog.Namespace).
+										Str("interaction", ssrcLog.Interaction).
+										Str("user", ssrcLog.User).
+										Uint64("lost", lost).
+										Uint64("count", count).
+										Msg(ssrcLog.Kind + "_in_report")
+								}
+							}
+						}
+					}
+				}
+			}
+		} else {
+			log.Logger.Trace().Msg(msg)
+		}
+	}
+	return
+}
+
+func (wc *logWriteCloser) Close() (err error) {
+	return
+}
--- engine/options.go
+++ engine/options.go
@@ -1,23 +1,17 @@
 package engine
 
 import (
-	"fmt"
-	"strconv"
-	"strings"
 	"time"
 
 	"github.com/ducksouplab/ducksoup/env"
-	"github.com/ducksouplab/ducksoup/store"
 	"github.com/pion/interceptor"
 	"github.com/pion/interceptor/pkg/cc"
 	"github.com/pion/interceptor/pkg/gcc"
 	"github.com/pion/interceptor/pkg/packetdump"
 	"github.com/pion/interceptor/pkg/twcc"
-	"github.com/pion/rtcp"
 	"github.com/pion/sdp/v3"
 	"github.com/pion/webrtc/v3"
 	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
 )
 
 // adapted from https://github.com/pion/webrtc/blob/v3.2.8/interceptor.go
@@ -29,16 +23,6 @@ func configureAPIOptions(m *webrtc.MediaEngine, r *interceptor.Registry, estimat
 
 	if err := webrtc.ConfigureRTCPReports(r); err != nil {
 		return err
-	}
-
-	if env.GCC {
-		// keep configurations here in that order
-		if err := configureEstimator(r, estimatorCh); err != nil {
-			return err
-		}
-	} else {
-		// needed not to block pc
-		estimatorCh <- nil
 	}
 
 	if err := webrtc.ConfigureTWCCHeaderExtensionSender(m, r); err != nil {
@@ -63,6 +47,16 @@ func configureAPIOptions(m *webrtc.MediaEngine, r *interceptor.Registry, estimat
 		if err := configurePacketDump(r); err != nil {
 			return err
 		}
+	}
+
+	if env.GCC {
+		// keep configurations here in that order
+		if err := configureEstimator(r, estimatorCh); err != nil {
+			return err
+		}
+	} else {
+		// needed not to block pc
+		estimatorCh <- nil
 	}
 
 	return nil
@@ -96,7 +90,14 @@ func configureEstimator(r *interceptor.Registry, estimatorCh chan cc.BandwidthEs
 // ConfigureTWCCSender will setup everything necessary for generating TWCC reports.
 func configureTWCCSender(m *webrtc.MediaEngine, r *interceptor.Registry) error {
 	m.RegisterFeedback(webrtc.RTCPFeedback{Type: webrtc.TypeRTCPFBTransportCC}, webrtc.RTPCodecTypeVideo)
+	if err := m.RegisterHeaderExtension(webrtc.RTPHeaderExtensionCapability{URI: sdp.TransportCCURI}, webrtc.RTPCodecTypeVideo); err != nil {
+		return err
+	}
+
 	m.RegisterFeedback(webrtc.RTCPFeedback{Type: webrtc.TypeRTCPFBTransportCC}, webrtc.RTPCodecTypeAudio)
+	if err := m.RegisterHeaderExtension(webrtc.RTPHeaderExtensionCapability{URI: sdp.TransportCCURI}, webrtc.RTPCodecTypeAudio); err != nil {
+		return err
+	}
 
 	generator, err := twcc.NewSenderInterceptor(twcc.SendInterval(30 * time.Millisecond))
 	if err != nil {
@@ -159,137 +160,16 @@ func configureSDESHeaderExtension(m *webrtc.MediaEngine) error {
 }
 
 func configurePacketDump(r *interceptor.Registry) error {
-	dumper, err := packetdump.NewSenderInterceptor(
-		packetdump.RTCPFormatter(formatSentRTCP),
+	rtcpDumperInterceptor, err := packetdump.NewSenderInterceptor(
+		packetdump.RTCPFormatter(rtcpFormatSent),
 		packetdump.RTCPWriter(&logWriteCloser{}),
 		packetdump.RTPWriter(zerolog.Nop()),
 	)
-
 	if err != nil {
 		return err
 	}
 
-	r.Add(dumper)
+	r.Add(rtcpDumperInterceptor)
+
 	return nil
-}
-
-// used for packet dumps
-
-func formatSentRTCP(pkts []rtcp.Packet, _ interceptor.Attributes) (res string) {
-	for _, pkt := range pkts {
-		switch rtcpPacket := pkt.(type) {
-		case *rtcp.TransportLayerNack:
-			res += fmt.Sprintf(
-				"[NACK] ssrc:%v sender:%v nacks:%v",
-				rtcpPacket.MediaSSRC,
-				rtcpPacket.SenderSSRC,
-				rtcpPacket.Nacks,
-			)
-		case *rtcp.TransportLayerCC:
-			var count uint16 = 0
-			var lost uint16 = 0
-			for _, chunk := range rtcpPacket.PacketChunks {
-				switch chk := chunk.(type) {
-				case *rtcp.RunLengthChunk:
-					count += chk.RunLength
-					if chk.PacketStatusSymbol == 0 {
-						lost += chk.RunLength
-					}
-				case *rtcp.StatusVectorChunk:
-					for _, symbol := range chk.SymbolList {
-						count += 1
-						if symbol == 0 {
-							lost += 1
-						}
-					}
-				}
-			}
-			res += fmt.Sprintf(
-				"[TWCC] ssrc:%v count:%v lost:%v",
-				rtcpPacket.MediaSSRC,
-				count,
-				lost,
-			)
-
-			// case *rtcp.ReceiverReport:
-			// 	res += "[RR sent] reports: "
-			// 	for _, report := range rtcpPacket.Reports {
-			// 		res += fmt.Sprintf("lost=%d/%d ", report.FractionLost, report.TotalLost)
-			// 	}
-			// default:
-			// 	res += fmt.Sprintf("[%T sent]", rtcpPacket)
-		}
-	}
-	return res
-}
-
-// func formatReceivedRTCP(pkts []rtcp.Packet, _ interceptor.Attributes) (res string) {
-// 	for _, pkt := range pkts {
-// 		res += fmt.Sprintf("[receiver] %T RTCP packet: %v\n", pkt, pkt)
-// 	}
-// 	return res
-// }
-
-// func formatReceivedRTP(pkt *rtp.Packet, attributes interceptor.Attributes) string {
-// 	var twcc rtp.TransportCCExtension
-// 	ext := pkt.GetExtension(pkt.GetExtensionIDs()[0])
-// 	twcc.Unmarshal(ext)
-
-// 	return fmt.Sprintf("[RTP] #%v now:%v ssrc:%v size:%v",
-// 		twcc.TransportSequence,
-// 		time.Now().Format("15:04:05.99999999"),
-// 		pkt.SSRC,
-// 		pkt.MarshalSize(),
-// 	)
-// }
-
-// used by RTCP log interceptors
-type logWriteCloser struct{}
-
-func (wc *logWriteCloser) Write(p []byte) (n int, err error) {
-	n = len(p)
-	if n > 0 {
-		msg := string(p)
-		if strings.HasPrefix(msg, "[TWCC]") {
-			ssrcMatch := ssrcRegexp.FindStringSubmatch(msg)
-			// trace level to respect DUCKSOUP_LOG_LEVEL setting
-			if len(ssrcMatch) > 0 {
-				// remove ssrc from string and add it as a log prop
-				msg = ssrcRegexp.ReplaceAllString(msg, "")
-
-				if ssrc64, err := strconv.ParseUint(ssrcMatch[1], 10, 32); err == nil {
-					ssrc := uint32(ssrc64)
-					ssrcLog := store.GetFromSSRCIndex(ssrc)
-					if ssrcLog != nil {
-						countMatch := countRegexp.FindStringSubmatch(msg)
-						lostMatch := lostRegexp.FindStringSubmatch(msg)
-
-						if len(countMatch) > 0 && len(lostMatch) > 0 {
-							// don't log empty counts
-							if count, err := strconv.ParseUint(countMatch[1], 10, 64); err == nil && count != 0 {
-								if lost, err := strconv.ParseUint(lostMatch[1], 10, 64); err == nil {
-									log.Logger.Trace().
-										Str("context", "track").
-										Str("ssrc", ssrcMatch[1]).
-										Str("namespace", ssrcLog.Namespace).
-										Str("interaction", ssrcLog.Interaction).
-										Str("user", ssrcLog.User).
-										Uint64("lost", lost).
-										Uint64("count", count).
-										Msg(ssrcLog.Kind + "_in_report")
-								}
-							}
-						}
-					}
-				}
-			}
-		} else {
-			log.Logger.Trace().Msg(msg)
-		}
-	}
-	return
-}
-
-func (wc *logWriteCloser) Close() (err error) {
-	return
 }
